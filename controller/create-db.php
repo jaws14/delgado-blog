153aadcbc8b41b6d536a81c5a5dfc05176aeb052
pablo j delgado <fluffydemonkey@gmail>
2015-01-29 09:18:56 -0800

    added code

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,4 +1,21 @@
 <?php
-    require_once("../model/database.php");
+    require_once(__DIR__ ."/../model/database.php");
     
-    $connection = new mysqli
+    $connection = new mysqli($host, $username, $password);
+    
+    if($connection->connect_error) {
+        die("Error: " . $connection->connect_error);
+    }
+  
+    $exists = $connection->select_db($database);
+    
+    if(!$exists) {
+        $query = $connection->query("CREATE DATABASE $database");
+        
+        if($query) {
+           echo "successfully created database" 
+        }
+    }
+    
+    
+    $connection->close();
