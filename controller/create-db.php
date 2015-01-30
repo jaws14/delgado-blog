8dcdd2e8e758b1880cbf10f62e159a3b69733363
pablo j delgado <fluffydemonkey@gmail>
2015-01-30 09:19:30 -0800

    added code

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,24 +1,36 @@
 <?php
-    require_once(__DIR__ ."/../model/database.php");
-    
-    $connection = new mysqli($host, $username, $password);
-    
-    if($connection->connect_error) {
-        die("Error: " . $connection->connect_error);
+
+require_once(__DIR__ . "/../model/database.php");
+
+$connection = new mysqli($host, $username, $password);
+
+if ($connection->connect_error) {
+    die("<p>Error:" . $connection->connect_error . "</p>");
+}
+
+$exists = $connection->select_db($database);
+
+if (!$exists) {
+    $query = $connection->query("CREATE DATABASE $database");
+
+    if ($query) {
+        echo "<p>successfully created database " . $database . "</p>";
     }
-  
-    $exists = $connection->select_db($database);
-    
-    if(!$exists) {
-        $query = $connection->query("CREATE DATABASE $database");
-        
-        if($query) {
-           echo "successfully created database" . $database ;
-        
-        }
-    }
-    else {
-       echo "Data base has already"
-    }
-    
-    $connection->close();
+} else {
+    echo "<p>Database already exists.</p>";
+}
+
+$query = $connection->query("CREATE TABLE posts ("
+        . "id int(11) NOT NULL AUTO_INCREMENT, "
+        . "title varchar(255) NOT NULL,"
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");
+
+if($query) {
+    echo "<p>succesfully created table: posts</p>";
+}
+else {
+    echo "<p>$connection->error</p>";
+}
+
+$connection->close();
